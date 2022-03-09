d9d832b2accbfc46286298bc6f6fd2f774f30f41
Tarin <tarin.eccleston@gmail.com>
2022-03-10 12:50:30 +1300

    Tidy

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from skyfield.api import load
+import math
+
+def calculatePosition(sun, planet, t):
+    position = sun.at(t).observe(earth)
+    ra, dec, distance = position.radec()
+    
+    return ra, dec, distance
 
 ts = load.timescale()
 t = ts.now()
+# t = ts.utc(2022, 4, 7)
 
-planets = load('de421.bsp')  # ephemeris DE421
-sun, mercury, venus, earth, mars = planets['sun'], planets['mercury'], planets['venus'], planets['earth'], planets['mars']
+eph = load('de421.bsp')  # ephemeris DE421
+
+sun = eph['sun']
+mercury, venus, earth, mars = {eph['mercury barycenter'], eph['venus barycenter'], eph['earth barycenter'], eph['mars barycenter']}
+
+ra, dec, distance = calculatePosition(sun, mercury, t)
 
 
